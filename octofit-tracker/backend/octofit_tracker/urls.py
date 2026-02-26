c3a6c33b7fde39380e8bf444e44cc37261eb369b
ohogoschcompass <ricardo.gosch@compasso.com.br>
2026-02-26 18:33:40 +0000

    Ajustar rotas da API para incluir o caminho 'admin/' e corrigir a nomenclatura do endpoint 'api-root'

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -59,6 +59,7 @@ def api_root(request):
     })
 
 urlpatterns = [
-    path('', api_root, name='api_root'),
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', api_root, name='api-root'),
+    path('api/', include(router.urls)),
 ]
